093c4de5d645ba086cd06ad0969f76461a8365a8
Ian Dickerson <ian@ada.ac.uk>
2020-07-28 16:29:17 +0100

    subject graph can cover multiple cohorts

--- pages/subject.py
+++ pages/subject.py
@@ -18,11 +18,13 @@ layout = html.Div([
     [Input('cohort-dropdown', 'value'),
      Input('subject-dropdown', 'value'),
      Input('point-dropdown', 'value')])
-def update_figure(cohort_value, subject_value, point_value):
+def update_figure(cohort_values, subject_value, point_value):
     db = get_db()
+    if type(cohort_values) != list:
+        cohort_values = [cohort_values]
     assessments = [r['doc'] for r in db.get_view_result('assessment', 'subject', key=subject_value, include_docs=True).all()]
     assessments_df = pd.DataFrame.from_records(assessments)
-    students = [r['doc'] for r in db.get_view_result('enrolment', 'cohort', key=cohort_value, include_docs=True).all()]
+    students = [r['doc'] for r in db.get_view_result('enrolment', 'cohort', keys=cohort_values, include_docs=True).all()]
     students_df = pd.DataFrame.from_records(students)
     df = assessments_df.merge(students_df, how='left', left_on='student_id', right_on='_id').query(f'point == "{point_value}"')
     figure = {
@@ -36,7 +38,7 @@ def update_figure(cohort_value, subject_value, point_value):
             go.Layout(
                 yaxis={'categoryorder': "array", 'categoryarray': ['U','E','D','C','B','A','S']},
                 hovermode='closest',
-                title=f'Cohort {cohort_value} | {subject_value} | {point_value}'
+                title=f'Cohort {cohort_values} | {subject_value} | {point_value}'
             )
         }
     return figure
