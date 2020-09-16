f73cda5908e5eff4d3b8e9ca745d93747d0bd022
Ian Dickerson <ian@ada.ac.uk>
2020-09-16 12:48:00 +0000

    google oauth initial implementation
    needs dash-google-auth latest to set redirect_url

--- dashboard.py
+++ dashboard.py
@@ -2,22 +2,45 @@
 Contains the dash app definition 
 """
 # dash
+import os
 from flask import Flask
 import dash
 import dash_bootstrap_components as dbc
+from dash_google_auth import GoogleOAuth
+from configparser import ConfigParser
 import index
 import store
 import dialog
 import dispatch
 from pages import cohort, team, subject, student
 
+config_object = ConfigParser()
+config_object.read("config.ini")
+oauth_config = config_object["OAUTH"]
+client_id = oauth_config["CLIENT_ID"]
+client_secret = oauth_config["CLIENT_SECRET"]
+flask_secret_key = oauth_config["FLASK_SECRET_KEY"]
+authorized_emails = oauth_config["AUTHORIZED_EMAILS"]
+
+server = Flask(__name__)
+server.config.update({
+    "GOOGLE_OAUTH_CLIENT_ID": client_id,
+    "GOOGLE_OAUTH_CLIENT_SECRET": client_secret
+})
+server.secret_key = flask_secret_key
+
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = '1'
+
 # create dash app
 app = dash.Dash(
     __name__,
+    server=server,
     suppress_callback_exceptions=False,
     #external_stylesheets=["https://codepen.io/chriddyp/pen/bWLwgP.css"],
     external_stylesheets=[dbc.themes.FLATLY],
 )
+
+auth = GoogleOAuth(app, authorized_emails, ['openid'], "172.26.14.11/login/google/authorized")
 
 app.layout = index.layout
 index.register_callbacks(app)
