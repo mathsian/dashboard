243fd53fb5b19a54cae48a04e5b52cde3fe7b938
Ian Dickerson <ian@ada.ac.uk>
2020-09-30 10:20:34 +0000

    rationalise registering callbacks for readability

--- dashboard.py
+++ dashboard.py
@@ -1,19 +1,22 @@
 """
-Contains the dash app definition 
+Instantiates the dash app and registers callbacks for all sections 
 """
-# dash
 import os
 from flask import Flask
 import dash
 import dash_bootstrap_components as dbc
+# Third party library for authenticating with google
+# https://github.com/mathsian/dash-google-auth
 from dash_google_auth import GoogleOAuth
+
 from configparser import ConfigParser
-import index
-import store
-import dispatch
+
+# Layouts and callbacks for the tabs/pages
+import index, store, dispatch
 from pages import cohort, team, subject, student
 from forms import kudos, concern
 
+# Get configuration
 config_object = ConfigParser()
 config_object.read("config.ini")
 oauth_config = config_object["OAUTH"]
@@ -21,6 +24,7 @@ client_id = oauth_config["CLIENT_ID"]
 client_secret = oauth_config["CLIENT_SECRET"]
 authorized_emails = oauth_config["AUTHORIZED_EMAILS"]
 
+# We need the flask instance to run google oauth
 server = Flask(__name__)
 server.config.update({
     "GOOGLE_OAUTH_CLIENT_ID": client_id,
@@ -28,6 +32,7 @@ server.config.update({
 })
 server.secret_key = os.urandom(24) 
 
+# Once we have https we can remove this in production
 os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = '1'
 
 # create dash app
@@ -39,18 +44,16 @@ app = dash.Dash(
     #external_stylesheets=["https://codepen.io/chriddyp/pen/bWLwgP.css"],
     external_stylesheets=[dbc.themes.FLATLY],
 )
-
+# wrap in google oauth
 auth = GoogleOAuth(app, authorized_emails, ['openid'])
 
+# register the layout
+# index.layout contains the layouts of all the other sections
 app.layout = index.layout
-index.register_callbacks(app)
-store.register_callbacks(app)
-cohort.register_callbacks(app)
-team.register_callbacks(app)
-subject.register_callbacks(app)
-student.register_callbacks(app)
-dispatch.register_callbacks(app)
-kudos.register_callbacks(app)
-concern.register_callbacks(app)
+# register the callbacks
+for section in [index, store, dispatch, cohort, team, subject, student, kudos, concern]:
+    section.register_callbacks(app)
+
 if __name__ == "__main__":
-    app.run_server(debug=True, port=8000)
+    # If we're running via __main__ then we're in development
+    app.run_server(debug=True, port=8001)
