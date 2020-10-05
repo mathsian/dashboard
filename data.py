fabbfaf1ba916bf90fed3641c3dd8c5117bd2a78
Ian Dickerson <ian@ada.ac.uk>
2020-10-05 08:53:33 +0000

    example usage

--- data.py
+++ data.py
@@ -66,6 +66,12 @@ def get_data(doc_type, key_field, key_list, db_name="testing"):
     Get all docs of given data_type for a list of keys key_list
 
     key_list may be a single object in which case it is converted to a singleton list
+
+    Examples:
+
+    get_data("enrolment", "student_id", "160001")
+
+    get_data("all", "type", "enrolment")
     """
     if not isinstance(key_list, list):
         key_list = [key_list]
@@ -74,7 +80,6 @@ def get_data(doc_type, key_field, key_list, db_name="testing"):
             doc_type, key_field, keys=key_list, include_docs=True
         ).all()
     return list(map(lambda r: r["doc"], result))
-
 
 def get_student(student_id, db_name="testing"):
     with Connection(db_name) as db:
