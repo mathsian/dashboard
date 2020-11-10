f4c6d811f3a7f929573e96738c0d37e61ef60202
Ian Dickerson <ian@ada.ac.uk>
2020-11-10 12:37:23 +0000

    connection settings in config

--- admin.py
+++ admin.py
@@ -1,60 +1,76 @@
 import pyodbc
 import data
 import pandas as pd
+from configparser import ConfigParser
 import curriculum
 
+
 def initial_sync(dbname):
-    conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=172.26.11.131;DATABASE=Reports;UID=Reader;PWD=Reader1')
-    sql= "SELECT T1.STEM_Student_ID, T2.STUD_Known_As, T1.STUD_Surname, T1.Group_ID, T1.Group_Code, T1.Group_Name FROM Reports.dbo.Vw_Enr_Current_SixthForm AS T1 LEFT JOIN REMSLive.dbo.STUDstudent AS T2 ON T1.STEM_Student_ID = T2.STUD_Student_ID;"
+    config_object = ConfigParser()
+    config_object.read("config.ini")
+    rems_settings = config_object["REMS"]
+    rems_server = rems_settings["ip"]
+    rems_uid = rems_settings["uid"]
+    rems_pwd = rems_settings["pwd"]
+    conn = pyodbc.connect(
+        f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={rems_server};DATABASE=Reports;UID={rems_uid};PWD={rems_pwd}'
+    )
+    sql = "SELECT T1.STEM_Student_ID, T2.STUD_Known_As, T1.STUD_Surname, T1.Group_ID, T1.Group_Code, T1.Group_Name FROM Reports.dbo.Vw_Enr_Current_SixthForm AS T1 LEFT JOIN REMSLive.dbo.STUDstudent AS T2 ON T1.STEM_Student_ID = T2.STUD_Student_ID;"
     df = pd.read_sql(sql, conn)
-    students= [
-            {
-                "_id": st.get("STEM_Student_ID"),
-                "type": "enrolment",
-                "given_name": st.get("STUD_Known_As"),
-                "family_name": st.get("STUD_Surname"),
-                "cohort": "1921" if "Yr 13" in st.get("Group_Name") else "2022",
-                "team": st.get("Group_Name")[30:],
-                "aps": 0,
-            }
-            for st in df[df["Group_Name"].str.contains("Team Time", na=False)].to_dict(orient='records')
-        ]
-    groups = [
-        {
-            "group_id": str(int(st.get("Group_ID"))),
-            "type": "group",
-            "code": st.get("Group_Code"),
-            "name": st.get("Group_Name"),
-            "student_id": st.get("STEM_Student_ID"),
-        }
-        for st in df[~df["Group_Name"].str.contains("EEP", na=True)].to_dict(orient='records')
-    ]
+    students = [{
+        "_id": st.get("STEM_Student_ID"),
+        "type": "enrolment",
+        "given_name": st.get("STUD_Known_As"),
+        "family_name": st.get("STUD_Surname"),
+        "cohort": "1921" if "Yr 13" in st.get("Group_Name") else "2022",
+        "team": st.get("Group_Name")[30:],
+        "aps": 0,
+    } for st in df[df["Group_Name"].str.contains(
+        "Team Time", na=False)].to_dict(orient='records')]
+    groups = [{
+        "group_id": str(int(st.get("Group_ID"))),
+        "type": "group",
+        "code": st.get("Group_Code"),
+        "name": st.get("Group_Name"),
+        "student_id": st.get("STEM_Student_ID"),
+    } for st in df[~df["Group_Name"].str.contains("EEP", na=True)].to_dict(
+        orient='records')]
     data.save_docs(groups, dbname)
     data.save_docs(students, dbname)
 
 
 def create_assessment_all(cohort, name, date, scale_name, dbname):
-    student_df = pd.DataFrame.from_records(data.get_data("enrolment", "cohort",
-        cohort, dbname))
+    student_df = pd.DataFrame.from_records(
+        data.get_data("enrolment", "cohort", cohort, dbname))
     student_ids = list(student_df["_id"].unique())
     groups = data.get_data("group", "student_id", student_ids, dbname)
-    assessments = [
-        {"type": "assessment",
-         "subtype": scale_name,
-         "assessment": name,
-         "date": date,
-         "comment": "",
-         "student_id": g.get("student_id"),
-            "subject": g.get("name"),
-            "cohort": cohort,
-            "grade": curriculum.scales.get(scale_name)[2]} for g in groups]
+    assessments = [{
+        "type": "assessment",
+        "subtype": scale_name,
+        "assessment": name,
+        "date": date,
+        "comment": "",
+        "student_id": g.get("student_id"),
+        "subject": g.get("name"),
+        "cohort": cohort,
+        "grade": curriculum.scales.get(scale_name)[2]
+    } for g in groups]
     data.save_docs(assessments, dbname)
 
 
 def get_weekly_attendance(wb, dbname):
-    conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=172.26.11.131;DATABASE=Reports;UID=Reader;PWD=Reader1')
+    config_object = ConfigParser()
+    config_object.read("config.ini")
+    rems_settings = config_object["REMS"]
+    rems_server = rems_settings["ip"]
+    rems_uid = rems_settings["uid"]
+    rems_pwd = rems_settings["pwd"]
+    conn = pyodbc.connect(
+        f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={rems_server};DATABASE=Reports;UID={rems_uid};PWD={rems_pwd}'
+    )
     sql = f"SELECT [REGD_Attendance_Mark], [REGD_Student_ID] FROM [Reports].[dbo].[Vw_Rpt_Marks] WHERE [Wk_Start] = '{wb} 00:00:00.000';"
-    df = pd.read_sql(sql, conn).groupby('REGD_Student_ID').agg({'REGD_Attendance_Mark': ''.join})
+    df = pd.read_sql(sql, conn).groupby('REGD_Student_ID').agg(
+        {'REGD_Attendance_Mark': ''.join})
     df['possible'] = df['REGD_Attendance_Mark'].apply(len)
     df['late'] = df['REGD_Attendance_Mark'].str.count('L')
     df['unauthorised'] = df['REGD_Attendance_Mark'].str.count('N')
@@ -62,9 +78,11 @@ def get_weekly_attendance(wb, dbname):
     df['type'] = 'attendance'
     df['date'] = wb
     # bring index back as column and rename to fit conventions
-    data.save_docs(df.reset_index().rename(columns={'REGD_Attendance_Mark':
-        'marks', 'REGD_Student_ID': 'student_id'}).to_dict(orient='records'),
-        dbname)
+    data.save_docs(
+        df.reset_index().rename(columns={
+            'REGD_Attendance_Mark': 'marks',
+            'REGD_Student_ID': 'student_id'
+        }).to_dict(orient='records'), dbname)
 
 
 def copy_docs(doc_type, db_src, db_dest):
@@ -75,11 +93,13 @@ def copy_docs(doc_type, db_src, db_dest):
         r.pop("_rev", None)
     data.save_docs(result, db_name=db_dest)
 
+
 if __name__ == "__main__":
     initial_sync("ada")
     copy_docs("kudos", "testing", "ada")
     copy_docs("concern", "testing", "ada")
-    create_assessment_all("2022", "First seven weeks", "2020-10-13", "Expectations", "ada")
+    create_assessment_all("2022", "First seven weeks", "2020-10-13",
+                          "Expectations", "ada")
     get_weekly_attendance("2020-08-31", "ada")
     get_weekly_attendance("2020-09-07", "ada")
     get_weekly_attendance("2020-09-14", "ada")
