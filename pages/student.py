644a37d2316e89139166c1142e3bc1294fa27972
Ian Dickerson <ian@ada.ac.uk>
2020-11-11 18:26:30 +0000

    implement student table

--- pages/student.py
+++ pages/student.py
@@ -2,64 +2,112 @@ import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output, State, ALL
+import dash_table
+import pandas as pd
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
 from app import app
+import data
+import curriculum
 
-name = "student"
+# The student list is on a separate card alongside the tabs
+# so that it isn't updated when the tab changes
+student_list = dash_table.DataTable(
+    id={
+        "type": "table",
+        "page": "student"
+    },
+    columns=[
+        {
+            "name": "Given Name",
+            "id": "given_name"
+        },
+        {
+            "name": "Family Name",
+            "id": "family_name"
+        },
+    ],
+    style_table={
+        "overflowY": "auto",
+        "height": "80vh",
+    },
+    style_cell={
+        "textAlign": "left",
+        "overflow": "hidden",
+    },
+    style_as_list_view=True,
+    row_selectable="multi",
+    sort_action="native",
+    filter_action="native",
+    sort_by=[{
+        "column_id": "given_name",
+        "direction": "asc"
+    }],
+)
 
+tabs = ["Report", "Kudos", "Concern"]
 content = [
-    dbc.Card(
-        [
-            dbc.CardHeader(
-                dbc.Tabs(
-                    [
-                        dbc.Tab(label=f"{name}-tab-{t}", tab_id=f"{name}-tab-{t}")
-                        for t in ["a", "b", "c"]
-                    ],
-                    id=f"{name}-tabs",
-                    card=True,
-                    active_tab=f"{name}-tab-a",
-                )
-            ),
-            dbc.CardBody(
-                dbc.Row(
-                    [
-                        dbc.Col(width=3, children=html.Div(id=f"{name}-sidebar")),
-                        dbc.Col(width=7, children=html.Div(id=f"{name}-main")),
-                        dbc.Col(width=2, children=html.Div(id=f"{name}-panel")),
-                    ]
-                )
-            ),
-        ]
-    )
+    dbc.Row([
+        dbc.Col(
+            width=4,
+            children=dbc.Card([
+                dbc.CardHeader("Students"),
+                dbc.CardBody(children=student_list)
+            ],
+                              style={"height": "95vh"}),
+        ),
+        dbc.Col(
+            width=6,
+            children=dbc.Card([
+                dbc.CardHeader(
+                    dbc.Tabs(
+                        [
+                            dbc.Tab(label=t, tab_id=f"student-tab-{t.lower()}")
+                            for t in tabs
+                        ],
+                        id=f"student-tabs",
+                        card=True,
+                        active_tab=f"student-tab-{tabs[0].lower()}",
+                    )),
+                dbc.CardBody(id=f"student-content", children=[]) ,
+            ]))
+    ])
 ]
 
 validation_layout = content
+tab_map = {
+    "student-tab-report": ["Report content"],
+    "student-tab-kudos": ["Kudos content"],
+    "student-tab-concern": ["Concern content"],
+}
+
 
 @app.callback(
-    [
-        Output(f"{name}-sidebar", "children"),
-        Output(f"{name}-main", "children"),
-        Output(f"{name}-panel", "children"),
-    ],
-    [Input(f"{name}-tabs", "active_tab"),
-     Input({"type": "filter-dropdown", "filter": ALL}, "value"),
-     Input("store-data", "data")],
+    Output(f"student-content", "children"),
+    [Input(f"student-tabs", "active_tab")],
 )
-def get_content(active_tab, filter_value, store_data):
-    return [
-        get_sidebar(active_tab, filter_value, store_data),
-        get_main(active_tab, filter_value, store_data),
-        get_panel(active_tab, filter_value, store_data),
-    ]
+def get_content(active_tab):
+    return tab_map.get(active_tab)
 
 
-def get_sidebar(active_tab, filter_value, store_data):
-    return f"{name} {active_tab} sidebar"
-
-
-def get_main(active_tab, filter_value, store_data):
-    return f"{name} {active_tab} {filter_value} {store_data}"
-
-
-def get_panel(active_tab, filter_value, store_data):
-    return f"{name} {active_tab} panel"
+@app.callback(
+    Output({
+        "type": "table",
+        "page": "student"
+    }, "data"),
+    [Input({
+        "type": "filter-dropdown",
+        "filter": ALL
+    }, "value")],
+)
+def update_student_table(filter_value):
+    cohort, team, _ = filter_value
+    if cohort and team:
+        enrolment_docs = data.get_data("enrolment", "cohort_team",
+                                       (cohort, team))
+    elif cohort:
+        enrolment_docs = data.get_data("enrolment", "cohort", cohort)
+    else:
+        return []
+    return enrolment_docs
