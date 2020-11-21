a7c98e9574e02d5079b118978c0f5a5aca380ecb
Ian Dickerson <ian@ada.ac.uk>
2020-11-21 10:04:00 +0000

    try dash daq components

--- pages/summary.py
+++ pages/summary.py
@@ -1,64 +1,84 @@
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
+import dash_daq as daq
 from dash.dependencies import Input, Output, State, ALL
+import pandas as pd
+
 from app import app
+import data
 
-name = "summary"
-
+tabs = ["Attendance", "Kudos"]
 content = [
-    dbc.Card(
-        [
-            dbc.CardHeader(
-                dbc.Tabs(
-                    [
-                        dbc.Tab(label=f"{name}-tab-{t}", tab_id=f"{name}-tab-{t}")
-                        for t in ["a", "b", "c"]
-                    ],
-                    id=f"{name}-tabs",
-                    card=True,
-                    active_tab=f"{name}-tab-a",
-                )
-            ),
-            dbc.CardBody(
-                dbc.Row(
-                    [
-                        dbc.Col(width=3, children=html.Div(id=f"{name}-sidebar")),
-                        dbc.Col(width=7, children=html.Div(id=f"{name}-main")),
-                        dbc.Col(width=2, children=html.Div(id=f"{name}-panel")),
-                    ]
-                )
-            ),
-        ]
-    )
+    dbc.Card([
+        dbc.CardHeader(
+            dbc.Tabs(
+                [
+                    dbc.Tab(label=t, tab_id=f"summary-tab-{t.lower()}")
+                    for t in tabs
+                ],
+                id=f"summary-tabs",
+                card=True,
+                active_tab=f"summary-tab-{tabs[0].lower()}",
+            )),
+        dbc.CardBody(id="summary-content", ),
+    ]),
+    dcc.Interval(id={
+        "type": "interval",
+        "page": "summary",
+        "tab": "attendance"
+    },
+                 interval=60_000,
+                 n_intervals=0),
 ]
+attendance_dashboard = dbc.Container(children=[
+    daq.Gauge(
+        id={
+            "type": "gauge",
+            "page": "summary",
+            "tab": "attendance",
+            "name": "cumulative"
+        },
+        label="Cumulative attendance",
+        scale={
+            "start": 0,
+            "interval": 5,
+            "labelInterval": 2,
+        },
+        value=0,
+        min=0,
+        max=100,
+    )
+])
 
-validation_layout = content
+validation_layout = content + [attendance_dashboard]
+tab_map = {"summary-tab-attendance": [attendance_dashboard]}
+
+
+@app.callback([
+    Output(f"summary-content", "children"),
+], [Input(f"summary-tabs", "active_tab")])
+def get_content(active_tab):
+    return tab_map.get(active_tab)
+
+
 @app.callback(
-    [
-        Output(f"{name}-sidebar", "children"),
-        Output(f"{name}-main", "children"),
-        Output(f"{name}-panel", "children"),
-    ],
-    [Input(f"{name}-tabs", "active_tab"),
-     Input({"type": "filter-dropdown", "filter": ALL}, "value"),
-     Input("store-data", "data")],
-)
-def get_content(active_tab, filter_value, store_data):
-    return [
-        get_sidebar(active_tab, filter_value, store_data),
-        get_main(active_tab, filter_value, store_data),
-        get_panel(active_tab, filter_value, store_data),
-    ]
-
-
-def get_sidebar(active_tab, filter_value, store_data):
-    return f"{name} {active_tab} sidebar"
-
-
-def get_main(active_tab, filter_value, store_data):
-    return f"{name} {active_tab} {filter_value} {store_data}"
-
-
-def get_panel(active_tab, filter_value, store_data):
-    return f"{name} {active_tab} panel"
+    Output(
+        {
+            "type": "gauge",
+            "page": "summary",
+            "tab": "attendance",
+            "name": "cumulative"
+        }, "value"), [
+            Input({
+                "type": "interval",
+                "page": "summary",
+                "tab": "attendance"
+            }, "n_intervals"),
+        ])
+def update_attendance_gauge(n_intervals):
+    attendance_docs = data.get_data("all", "type", "attendance")
+    actual = sum([a.get('actual') for a in attendance_docs])
+    possible = sum([a.get('possible') for a in attendance_docs])
+    percent = round(100 * actual / possible)
+    return percent
