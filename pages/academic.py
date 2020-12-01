9b60df6688929517b6e2ec24b66427d576a4bc2f
Ian Dickerson <ian@ada.ac.uk>
2020-12-01 10:00:51 +0000

    result edit table

--- pages/academic.py
+++ pages/academic.py
@@ -2,45 +2,104 @@ import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output, State, ALL
+import dash_table
+import pandas as pd
 from app import app
+import data
+import plotly.graph_objects as go
+import curriculum
 
 tabs = ["View", "Edit"]
 content = [
-    dbc.Card(
-        [
-            dbc.CardHeader(
-                dbc.Tabs(
-                    [
-                        dbc.Tab(label=t, tab_id=f"academic-tab-{t.lower()}")
-                        for t in tabs
-                    ],
-                    id=f"academic-tabs",
-                    card=True,
-                    active_tab=f"academic-tab-{tabs[0].lower()}",
-                )
-            ),
-            dbc.CardBody(
-                dbc.Row(
-                    [
-                        dbc.Col(width=3, children=html.Div(id=f"academic-sidebar")),
-                        dbc.Col(width=7, children=html.Div(id=f"academic-main")),
-                        dbc.Col(width=2, children=html.Div(id=f"academic-panel")),
-                    ]
-                )
-            ),
-        ]
-    )
+    dbc.Card([
+        dbc.CardHeader(
+            dbc.Tabs(
+                [
+                    dbc.Tab(label=t, tab_id=f"academic-tab-{t.lower()}")
+                    for t in tabs
+                ],
+                id=f"academic-tabs",
+                card=True,
+                active_tab=f"academic-tab-{tabs[0].lower()}",
+            )),
+        dbc.CardBody(dbc.Row(id="academic-content")),
+    ])
 ]
+subject_table = dash_table.DataTable(id={
+    "type": "table",
+    "page": "academic",
+    "tab": "edit"
+},
+                                     columns=[
+                                         {
+                                             "name": "Given name",
+                                             "id": "given_name",
+                                             "editable": False
+                                         },
+                                         {
+                                             "name": "Family name",
+                                             "id": "family_name",
+                                             "editable": False
+                                         },
+                                         {
+                                             "name": "Grade",
+                                             "id": "grade",
+                                             "type": "text",
+                                             "presentation": "dropdown",
+                                             "editable": True
+                                         },
+                                         {
+                                             "name": "Comment",
+                                             "id": "comment",
+                                             "type": "text",
+                                             "presentation": "input",
+                                             "editable": True
+                                         },
+                                     ],
+                                     sort_by=[{
+                                         "column_id": "given_name",
+                                         "direction": "asc"
+                                     }, {
+                                         "column_id": "family_name",
+                                         "direction": "asc"
+                                     }])
 
-validation_layout = content
+validation_layout = content + [subject_table]
+tab_map = {"academic-tab-view": [dbc.Col()], "academic-tab-edit": [dbc.Col(subject_table)]}
+
 
 @app.callback(
+    Output(f"academic-content", "children"),
     [
-        Output(f"academic-sidebar", "children"),
-        Output(f"academic-main", "children"),
-        Output(f"academic-panel", "children"),
+        Input(f"academic-tabs", "active_tab"),
     ],
-    [Input(f"academic-tabs", "active_tab"),],
 )
 def get_content(active_tab):
-    return "", "", ""
+    return tab_map.get(active_tab)
+
+
+@app.callback([
+    Output({
+        "type": "table",
+        "page": "academic",
+        "tab": "edit"
+    }, "data"),
+    Output({
+        "type": "table",
+        "page": "academic",
+        "tab": "edit"
+    }, "dropdown"),
+], [Input({
+    "type": "filter-dropdown",
+    "filter": ALL
+}, "value")])
+def update_subject_table(filter_value):
+    cohort, _, subject = filter_value
+    if not (cohort and subject):
+        return [], {}
+    group_df = pd.DataFrame.from_records(
+        data.get_data("group", "cohort_subject", [(cohort, subject)]))
+    student_ids = group_df["student_id"].tolist()
+    enrolment_df = pd.DataFrame.from_records(
+        data.get_data("enrolment", "_id", student_ids))
+    return enrolment_df.to_dict(orient='records'), {}
