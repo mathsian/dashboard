650da3a7415cb308c102ec43c429e4722d12a5c1
Ian Dickerson <ian@ada.ac.uk>
2021-02-02 11:21:22 +0000

    format updates

--- static_report.py
+++ static_report.py
@@ -16,25 +16,23 @@ def generate_report(student_id):
             a["grade"] = a.get("grade").replace("S", "A*")
         # Escape manually for now
         a["comment"] = a.get("comment").replace('%', '\%').replace('&', '\&')
+    academic_multiindex = pd.DataFrame.from_records(academic).set_index(["subject_name", "assessment"])[["grade", "date", "comment"]]
+    academic_dict = {subject: academic_multiindex.xs(subject).to_dict('index') for subject in academic_multiindex.index.levels[0]}
     kudos = data.get_data("kudos", "student_id", [student_id], "ada")
     kudos_total = 0
     for k in kudos:
-        k["description"] = k.get("description").replace('%', '\%').replace('&', '\&')
+        k["description"] = k.get("description", "").replace('%', '\%').replace('&', '\&')
         k['date'] = data.format_date(k['date'])
         kudos_total += k['points']
     concern = data.get_data("concern", "student_id", [student_id], "ada")
     concern_total = len(concern)
     for c in concern:
-        c["description"] = c.get("description").replace('%', '\%').replace('&', '\&')
+        c["description"] = c.get("description", "").replace('%', '\%').replace('&', '\&')
         c['date'] = data.format_date(c['date'])
-    attendance = data.get_data("attendance", "student_id", [student_id], "ada")
+    attendance = data.get_data("monthly", "student_id", [student_id], "ada")
     student = data.get_student(student_id, "ada")
 
     attendance_df = pd.DataFrame.from_records(attendance).sort_values(by='date', ascending=True)
-    # KLUDGE trim off latest week's attendance KLUDGE
-    max_date = attendance_df["date"].max()
-    min_date = "2020-11-02"
-    attendance_df = attendance_df.query("date < @max_date and date >= @min_date")
     attendance_df['date'] = attendance_df['date'].apply(data.format_date)
     attendance_df['percent'] = round(100*(attendance_df['actual']-attendance_df['late'])/attendance_df['possible'])
     attendance_df['late'] = round(100*attendance_df['late']/attendance_df['possible'])
@@ -57,16 +55,19 @@ def generate_report(student_id):
     student_name = f"{student.get('given_name')} {student.get('family_name')}"
     with open(f"latex/{student.get('_id')} {student_name}.tex", 'w') as f:
         template_data={"name": student_name,
+                       "date": "January 2020",
                        "attendance_dates": attendance_dates,
                        "attendance_zip": attendance_zip,
                        "punctuality_zip": punctuality_zip,
                        "attendance_cumulative": cumulative,
-                       "academic": academic,
+                       "academic": academic_dict,
                        "kudos": kudos,
                        "kudos_total": kudos_total,
                        "concerns": concern,
                        "concern_total": concern_total}
         f.write(template.render(template_data))
+
+
 def cohort_reports(cohort):
     enrolment_docs = data.get_data("enrolment", "cohort", cohort, db_name="ada")
     for student in enrolment_docs:
@@ -74,5 +75,9 @@ def cohort_reports(cohort):
         generate_report(student.get('_id'))
 
 if __name__ == "__main__":
-    generate_report("190765")
+    generate_report("201002")
+    generate_report("201003")
+    generate_report("190747")
+    generate_report("190723")
+    generate_report("190821")
     #cohort_reports("2022")
