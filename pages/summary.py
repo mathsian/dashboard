3fbf14d0302763874221e4a74165fd60d56958b5
Ian Dickerson <ian@ada.ac.uk>
2021-04-19 11:56:13 +0000

    query rems directly for the summart page

--- pages/summary.py
+++ pages/summary.py
@@ -40,16 +40,18 @@ content = [
                  n_intervals=0),
 ]
 attendance_dashboard = dbc.Container(children=[
-    dbc.Row([
-        dbc.Col(
-            daq.Gauge(
+    dbc.Row(
+        [
+            dbc.Col(daq.Gauge(
                 id={
                     "type": "gauge",
                     "page": "summary",
                     "tab": "attendance",
                     "name": "last"
                 },
-                label="This week",
+                label="This month",
+                labelPosition="bottom",
+                size=200,
                 scale={
                     "start": 0,
                     "interval": 5,
@@ -60,9 +62,9 @@ attendance_dashboard = dbc.Container(children=[
                 value=0,
                 min=0,
                 max=100,
-            )),
-        dbc.Col(
-            daq.Gauge(
+            ),
+                    width=4),
+            dbc.Col(daq.Gauge(
                 id={
                     "type": "gauge",
                     "page": "summary",
@@ -70,6 +72,8 @@ attendance_dashboard = dbc.Container(children=[
                     "name": "cumulative"
                 },
                 label="Overall",
+                labelPosition="bottom",
+                size=200,
                 scale={
                     "start": 0,
                     "interval": 5,
@@ -80,71 +84,79 @@ attendance_dashboard = dbc.Container(children=[
                 value=0,
                 min=0,
                 max=100,
-            ), )
-    ]),
-    dbc.Row([
-        dcc.Graph(id={
-            "type": "graph",
-            "page": "summary",
-            "tab": "attendance",
-            "name": "monthly"
-        },
-                  figure={
-                      "layout": {
-                          "xaxis": {
-                              "visible": False
-                          },
-                          "yaxis": {
-                              "visible": False
-                          }
-                      }
-                  },
-                  config={"displayModeBar": False}),
-        dcc.Graph(id={
-            "type": "graph",
-            "page": "summary",
-            "tab": "attendance",
-            "name": "low"
-        },
-                  figure={
-                      "layout": {
-                          "xaxis": {
-                              "visible": False
-                          },
-                          "yaxis": {
-                              "visible": False
-                          }
-                      }
-                  },
-                  config={"displayModeBar": False}),
-    ]),
-    html.Div([
-        dcc.Slider(
-            id={
-                "type": "slider",
+            ),
+                    width=4)
+        ],
+        align='center',
+        justify='center',
+    ),
+    dbc.Row(
+        dbc.Col([
+            dcc.Graph(id={
+                "type": "graph",
                 "page": "summary",
                 "tab": "attendance",
-                "name": "threshold",
+                "name": "monthly"
             },
-            min=0,
-            max=100,
-            value=92,
-            marks={
-                60: '60',
-                80: '80',
-                90: '90',
-                92: '92',
-                95: '95',
+                      figure={
+                          "layout": {
+                              "xaxis": {
+                                  "visible": False
+                              },
+                              "yaxis": {
+                                  "visible": False
+                              }
+                          }
+                      },
+                      config={"displayModeBar": False}),
+            dcc.Graph(id={
+                "type": "graph",
+                "page": "summary",
+                "tab": "attendance",
+                "name": "low"
             },
-        ),
-    ]),
-    html.Div(
-        id={
-            "type": "text",
-            "page": "summary",
-            "tab": "attendance",
-            "name": "low_cumulative"
-        }),
+                      figure={
+                          "layout": {
+                              "xaxis": {
+                                  "visible": False
+                              },
+                              "yaxis": {
+                                  "visible": False
+                              }
+                          }
+                      },
+                      config={"displayModeBar": False}),
+            html.Div([
+                dcc.Slider(
+                    id={
+                        "type": "slider",
+                        "page": "summary",
+                        "tab": "attendance",
+                        "name": "threshold",
+                    },
+                    min=0,
+                    max=100,
+                    value=92,
+                    marks={
+                        60: '60',
+                        80: '80',
+                        90: '90',
+                        92: '92',
+                        95: '95',
+                    },
+                ),
+            ]),
+            html.Div(
+                id={
+                    "type": "text",
+                    "page": "summary",
+                    "tab": "attendance",
+                    "name": "low_cumulative"
+                }),
+        ], ),
+        align='start',
+        justify='center',
+    )
 ])
 unauthorised_table = dash_tabulator.DashTabulator(
     id={
@@ -152,75 +164,67 @@ unauthorised_table = dash_tabulator.DashTabulator(
         "page": "summary",
         "tab": "unauthorised",
     },
-    options={"resizableColumns": False, "layout": "fitData"},
+    options={
+        "resizableColumns": False,
+        "layout": "fitData"
+    },
     theme='bootstrap/tabulator_bootstrap4',
-    columns=[
-        {
-            "title": "Date",
-            "field": "date"
-        },
-        {
-            "title": "Given name",
-            "field": "given_name",
-            "headerFilter": True,
-        },
-        {
-            "headerFilter": True,
-            "title": "Surname",
-            "field": "family_name"
-        },
-        {
-            "headerFilter": True,
-            "title": "Subject",
-            "field": "subject_code"
-        },
-        {
-            "title": "Day",
-            "field": "day"
-        },
-        {
-            "title": "Period",
-            "field": "period"
-        }
-    ]
-    )
+    columns=[{
+        "title": "Date",
+        "field": "date"
+    }, {
+        "title": "Given name",
+        "field": "given_name",
+        "headerFilter": True,
+    }, {
+        "headerFilter": True,
+        "title": "Surname",
+        "field": "family_name"
+    }, {
+        "headerFilter": True,
+        "title": "Subject",
+        "field": "subject_code"
+    }, {
+        "title": "Day",
+        "field": "day"
+    }, {
+        "title": "Period",
+        "field": "period"
+    }])
 missing_table = dash_tabulator.DashTabulator(
     id={
         "type": "table",
         "page": "summary",
         "tab": "missing",
     },
-    options={"resizableColumns": False, "layout": "fitData"},
+    options={
+        "resizableColumns": False,
+        "layout": "fitData"
+    },
     theme='bootstrap/tabulator_bootstrap4',
-    columns=[
-        {
-            "title": "Date",
-            "field": "date"
-        },
-        {
-            "title": "Register",
-            "field": "register"
-        },
-        {
-            "title": "Period",
-            "field": "period"
-        },
-        {
-            "title": "Lecturer",
-            "field": "lecturer"
-        }
-    ]
-    )
-validation_layout = content + [attendance_dashboard, unauthorised_table, missing_table]
-tab_map = {"summary-tab-attendance": [attendance_dashboard],
-           "summary-tab-unauthorised": [
-               dbc.Col(
-[html.H3("Last 10 days"), unauthorised_table]
-               )],
-           "summary-tab-missing": [
-               dbc.Col(
-               [html.H3("Missing registers"), missing_table])
-           ]}
+    columns=[{
+        "title": "Date",
+        "field": "date"
+    }, {
+        "title": "Register",
+        "field": "register"
+    }, {
+        "title": "Period",
+        "field": "period"
+    }, {
+        "title": "Lecturer",
+        "field": "lecturer"
+    }])
+validation_layout = content + [
+    attendance_dashboard, unauthorised_table, missing_table
+]
+tab_map = {
+    "summary-tab-attendance": [attendance_dashboard],
+    "summary-tab-unauthorised":
+    [dbc.Col([html.H3("Last 10 days"), unauthorised_table])],
+    "summary-tab-missing":
+    [dbc.Col([html.H3("Missing registers"), missing_table])]
+}
 
 
 @app.callback([
@@ -231,16 +235,17 @@ def get_content(active_tab):
 
 
 @app.callback(
-    Output(
-    {"type": "table",
-     "page": "summary",
-     "tab": "missing",
-     }, "data"),
-[Input(
-{"type": "interval",
- "page": "summary",
- "tab": "attendance"},
-"n_intervals")])
+    Output({
+        "type": "table",
+        "page": "summary",
+        "tab": "missing",
+    }, "data"), [
+        Input({
+            "type": "interval",
+            "page": "summary",
+            "tab": "attendance"
+        }, "n_intervals")
+    ])
 def update_missing_table(n_intervals):
     config_object = ConfigParser()
     config_object.read("config.ini")
@@ -252,8 +257,7 @@ def update_missing_table(n_intervals):
         f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={rems_server};DATABASE=Reports;UID={rems_uid};PWD={rems_pwd}'
     )
     sql_jinja_env = jinja2.Environment(
-        loader=jinja2.FileSystemLoader(abspath('.'))
-    )
+        loader=jinja2.FileSystemLoader(abspath('.')))
     sql_template = sql_jinja_env.get_template('sql/missing registers.sql')
     date_end = datetime.date.today()
     date_start = date_end - datetime.timedelta(days=10)
@@ -267,16 +271,17 @@ def update_missing_table(n_intervals):
 
 
 @app.callback(
-    Output(
-    {"type": "table",
-     "page": "summary",
-     "tab": "unauthorised",
-     }, "data"),
-[Input(
-{"type": "interval",
- "page": "summary",
- "tab": "attendance"},
-"n_intervals")])
+    Output({
+        "type": "table",
+        "page": "summary",
+        "tab": "unauthorised",
+    }, "data"), [
+        Input({
+            "type": "interval",
+            "page": "summary",
+            "tab": "attendance"
+        }, "n_intervals")
+    ])
 def update_unauthorised_table(n_intervals):
     config_object = ConfigParser()
     config_object.read("config.ini")
@@ -288,8 +293,7 @@ def update_unauthorised_table(n_intervals):
         f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={rems_server};DATABASE=Reports;UID={rems_uid};PWD={rems_pwd}'
     )
     sql_jinja_env = jinja2.Environment(
-        loader=jinja2.FileSystemLoader(abspath('.'))
-    )
+        loader=jinja2.FileSystemLoader(abspath('.')))
     sql_template = sql_jinja_env.get_template('sql/unauthorised absences.sql')
     date_end = datetime.date.today()
     date_start = date_end - datetime.timedelta(days=10)
@@ -300,7 +304,8 @@ def update_unauthorised_table(n_intervals):
     sql = sql_template.render(template_vars)
     df = pd.read_sql(sql, conn)
     return df.to_dict(orient='records')
- 
+
+
 @app.callback([
     Output(
         {
@@ -352,46 +357,42 @@ def update_unauthorised_table(n_intervals):
         }, "value")
 ])
 def update_attendance_gauge(n_intervals, threshold):
-    # All weekly attendance records
-    weekly_df = pd.DataFrame.from_records(
-        data.get_data("all", "type_subtype", [["attendance", "weekly"]]),
-        columns=["date", "student_id", "actual", "possible"])
-    # All monthly attendance records
-    monthly_df = pd.DataFrame.from_records(
-        data.get_data("all", "type_subtype", [["attendance", "monthly"]]),
-        columns=["date", "student_id", "actual", "possible"])
-    # For overall cumulative attendance
-    overall_sum = monthly_df.sum()
-    overall = round(100 * overall_sum['actual'] / overall_sum['possible'], 1)
-    # For this week
-    last_sum = weekly_df.query("date == date.max()").sum()
-    last = round(100 * last_sum['actual'] / last_sum['possible'], 1)
-    # For proportion of sts with cumulative low attendance
-    grouped_df = monthly_df.groupby("student_id").sum()
-    grouped_df.eval("percent = 100 * actual / possible", inplace=True)
-    low_cumulative = round(
-        100 * len(grouped_df.query("percent < @threshold")) / len(grouped_df),
-        1)
-    low_cumulative_text = f"{low_cumulative}% of students have < {threshold}% overall attendance"
+    config_object = ConfigParser()
+    config_object.read("config.ini")
+    rems_settings = config_object["REMS"]
+    rems_server = rems_settings["ip"]
+    rems_uid = rems_settings["uid"]
+    rems_pwd = rems_settings["pwd"]
+    conn = pyodbc.connect(
+        f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={rems_server};DATABASE=Reports;UID={rems_uid};PWD={rems_pwd}'
+    )
+    sql_jinja_env = jinja2.Environment(
+        loader=jinja2.FileSystemLoader(abspath('.')))
+    sql_template = sql_jinja_env.get_template(
+        'sql/attendance by student monthly with subtotals.sql')
+    sql = sql_template.render()
+    rems_df = pd.read_sql(sql, conn)
+    sql_template = sql_jinja_env.get_template(
+        'sql/cumulative attendance monthly.sql')
+    sql = sql_template.render()
+    cumulative_df = pd.read_sql(sql, conn)
+    low_cumulative_text = f"low_cumulative of students have < {threshold}% overall attendance"
     # For monthly graph
-    monthly_df.eval("percent = 100 * actual / possible", inplace=True)
-    monthly_df["low"] = np.where(monthly_df["percent"] < threshold, 1, 0)
-    monthly = monthly_df.groupby("date").agg({
-        "percent": 'mean',
-        "student_id": 'count',
-        "low": 'sum',
-    })
-    monthly.eval("low_percent = 100 * low / student_id", inplace=True)
-    monthly = monthly.round({'percent': 1, 'low_percent': 1})
-    monthly.reset_index(inplace=True)
+    monthly_df = rems_df.query('date != "Year" & student_id == "All"')
     monthly_figure = go.Figure(
         data=[
-            go.Bar(
-                x=monthly['date'],
-                y=monthly['percent'],
-                text=monthly['percent'],
-                textposition='auto',
-            ),
+            go.Bar(x=monthly_df['date'],
+                   y=monthly_df['attendance'],
+                   text=monthly_df['attendance'],
+                   textposition='auto',
+                   marker_color='steelblue',
+                   name="Monthly"),
+            go.Scatter(x=cumulative_df['date'],
+                       y=cumulative_df['cumulative'],
+                       text=cumulative_df['cumulative'],
+                       textposition='top center',
+                       marker_color='lightsteelblue',
+                       name="Cumulative"),
         ],
         layout={
             "title": "Monthly average student attendance",
@@ -400,17 +401,43 @@ def update_attendance_gauge(n_intervals, threshold):
             }
         },
     )
+    # For low attendance graph
+    low_df = rems_df.query("date != 'Year' and student_id != 'All'")
+    low_df["low"] = (low_df["attendance"] < threshold).astype('int')
+    low_grouped = low_df.groupby("date").agg({
+        "low": ["count", "sum"]
+    }).reset_index()
+    low_grouped['percent'] = round(
+        100 * low_grouped['low', 'sum'] / low_grouped['low', 'count'], 2)
+    low_grouped['low',
+                'cumcount'] = low_grouped['low',
+                                          'count'].transform(pd.Series.cumsum)
+    low_grouped['low',
+                'cumsum'] = low_grouped['low',
+                                        'sum'].transform(pd.Series.cumsum)
+    low_grouped['cumulative'] = round(
+        100 * low_grouped['low', 'cumsum'] / low_grouped['low', 'cumcount'], 2)
     low_figure = go.Figure(
         data=[
-            go.Bar(
-                x=monthly['date'],
-                y=monthly['low_percent'],
-                text=monthly['low_percent'],
-                textposition='auto',
-            ),
+            go.Bar(x=low_grouped['date'],
+                   y=low_grouped['percent'],
+                   text=low_grouped['percent'],
+                   textposition='auto',
+                   marker_color='steelblue',
+                   name="Monthly"),
+            go.Scatter(x=low_grouped['date'],
+                       y=low_grouped['cumulative'],
+                       text=low_grouped['cumulative'],
+                       textposition='top center',
+                       marker_color='lightsteelblue',
+                       name="Cumulative")
         ],
         layout={
             "title": f"Proportion of students with < {threshold}% attendance"
         },
     )
+    overall = rems_df.query(
+        'student_id == "All" & date == "Year"')["attendance"].iat[0]
+    last = rems_df.query('date != "Year"').query(
+        'student_id == "All" & date == date.max()')["attendance"].iat[0]
     return overall, last, monthly_figure, low_figure, low_cumulative_text
