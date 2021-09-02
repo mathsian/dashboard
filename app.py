d44cf201d06c02b62aa3af7f742b07b66d7fcfa5
Ian Dickerson <ian@ada.ac.uk>
2021-09-02 14:33:15 +0000

    full app with auth

--- app.py
+++ app.py
@@ -1,7 +1,30 @@
+import os
+from flask import Flask
 import dash
 import dash_bootstrap_components as dbc
+# Third party library for authenticating with google
+# https://github.com/mathsian/dash-google-auth
+from dash_google_auth import GoogleOAuth
+from configparser import ConfigParser
 
-app = dash.Dash(__name__,
-                external_stylesheets=[dbc.themes.BOOTSTRAP],
-                suppress_callback_exceptions=True)
-server = app.server
+# Get configuration
+config_object = ConfigParser()
+config_object.read("config.ini")
+oauth_config = config_object["OAUTH"]
+client_id = oauth_config["CLIENT_ID"]
+client_secret = oauth_config["CLIENT_SECRET"]
+authorized_emails = oauth_config["AUTHORIZED_EMAILS"]
+# We need the flask instance to run google oauth
+server = Flask(__name__)
+server.config.update({
+    "GOOGLE_OAUTH_CLIENT_ID": client_id,
+    "GOOGLE_OAUTH_CLIENT_SECRET": client_secret
+})
+server.secret_key = os.urandom(24) 
+# Once we have https we can remove this in production
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = '1'
+# Create dash app
+app = dash.Dash(__name__, server=server, external_stylesheets= [dbc.themes.LITERA], suppress_callback_exceptions=True)
+app.title = "data@ada"
+# wrap in google oauth
+auth = GoogleOAuth(app, authorized_emails, ['openid'])
