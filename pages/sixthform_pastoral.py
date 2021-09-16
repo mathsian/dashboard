803bf1743b9aba358865839f8aeccb8c49fd5511
Ian Dickerson <ian@ada.ac.uk>
2021-09-16 10:21:58 +0000

    move kudos radar to page sidebar

--- pages/sixthform_pastoral.py
+++ pages/sixthform_pastoral.py
@@ -5,21 +5,44 @@ import dash_core_components as dcc
 import dash_bootstrap_components as dbc
 import dash_html_components as html
 from dash.dependencies import Input, Output, State
+import pandas as pd
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 
 import data
+import curriculum
 
-cohort_dropdown = dbc.DropdownMenu(id="cohort-dropdown", nav=True, bs_size='sm')
+cohort_dropdown = dbc.DropdownMenu(id="cohort-dropdown",
+                                   nav=True,
+                                   bs_size='sm')
 team_dropdown = dbc.DropdownMenu(id="team-dropdown", nav=True, bs_size='sm')
 
-cardheader_layout = dbc.Nav(
-    [
-        dcc.Store(id="sixthform-pastoral", storage_type='memory'),
-        dbc.NavItem(cohort_dropdown),
-        dbc.NavItem(team_dropdown)
-    ], card=True
-)
+fig = make_subplots(specs=[[{"type": "polar"}]])
+fig.add_trace(
+    go.Scatterpolar(theta=curriculum.values,
+                    r=[0 for v in curriculum.values],
+                    subplot="polar",
+                    fill="toself"), 1, 1)
+fig.update_layout(polar=dict(radialaxis=dict(visible=False)), height=300)
 
-sidebar_layout = html.Div("SF pastoral sidebar")
+kudos_radar = dcc.Graph(id={
+    "type": "graph",
+    "section": "sixthform",
+    "page": "pastoral",
+    "name": "kudos",
+},
+                        config={"displayModeBar": False},
+                        figure=fig)
+
+cardheader_layout = dbc.Nav([
+    dcc.Store(id="sixthform-pastoral", storage_type='memory'),
+    dbc.NavItem(cohort_dropdown),
+    dbc.NavItem(team_dropdown)
+],
+                            card=True,
+                            fill=True)
+
+sidebar_layout = dbc.Container(kudos_radar)
 
 
 @app.callback([
@@ -54,14 +77,57 @@ def update_teams(pathname, search, team):
     student_ids = [e.get('_id') for e in enrolment_docs]
     attendance_docs = data.get_data("attendance", "student_id", student_ids)
     assessment_docs = data.get_data("assessment", "student_id", student_ids)
+    # Kudos processing
     kudos_docs = data.get_data("kudos", "student_id", student_ids)
+    kudos_df = pd.merge(pd.DataFrame.from_records(enrolment_docs),
+                        pd.DataFrame.from_records(kudos_docs),
+                        how="left",
+                        left_on="_id",
+                        right_on="student_id")
+    kudos_pivot_df = pd.pivot_table(
+        kudos_df,
+        values="points",
+        index=["student_id", "given_name", "family_name"],
+        columns="ada_value",
+        aggfunc=sum,
+        fill_value=0,
+    ).reindex(curriculum.values, axis=1, fill_value=0)
+    kudos_pivot_df["total"] = kudos_pivot_df.sum(axis=1)
+    kudos_pivot_df = kudos_pivot_df.reset_index()
+    kudos_pivot_docs = kudos_pivot_df.to_dict(orient='records')
+
     concern_docs = data.get_data("concern", "student_id", student_ids)
     store_data = {
         "student_ids": student_ids,
         "enrolment_docs": enrolment_docs,
         "attendance_docs": attendance_docs,
         "assessment_docs": assessment_docs,
-        "kudos_docs": kudos_docs,
+        "kudos_pivot_docs": kudos_pivot_docs,
         "concern_docs": concern_docs,
     }
     return (cohort, cohort_items, team, team_items, store_data)
+
+
+@app.callback(
+    Output({
+        "type": "graph",
+        "section": "sixthform",
+        "page": "pastoral",
+        "name": "kudos",
+    }, "figure"), [Input("sixthform-pastoral", "data")], [
+        State({
+            "type": "graph",
+            "section": "sixthform",
+            "page": "pastoral",
+            "name": "kudos",
+        }, "figure")
+    ])
+def update_kudos_radar(store_data, current_figure):
+    kudos_pivot_docs = store_data.get('kudos_pivot_docs')
+    if not kudos_pivot_docs:
+        current_figure["data"][0]["r"] = [0 for v in curriculum.values]
+        return [], current_figure
+    kudos_pivot_df = pd.DataFrame.from_records(kudos_pivot_docs)
+    r = [kudos_pivot_df[v].sum() for v in curriculum.values]
+    current_figure["data"][0]["r"] = r
+    return current_figure
--- pages/sixthform_pastoral_kudos.py
+++ pages/sixthform_pastoral_kudos.py
@@ -5,8 +5,6 @@ import dash_html_components as html
 import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output, State, ALL
 import pandas as pd
-import plotly.graph_objects as go
-from plotly.subplots import make_subplots
 import dash_daq as daq
 from datetime import date
 
@@ -18,8 +16,9 @@ import curriculum
 kudos_table = dash_tabulator.DashTabulator(
     id={
         "type": "table",
+        "section": "sixthform",
         "page": "pastoral",
-        "tab": "kudos",
+        "name": "kudos",
     },
     theme='bootstrap/tabulator_bootstrap4',
     options={
@@ -50,68 +49,20 @@ kudos_table = dash_tabulator.DashTabulator(
     }],
 )
 
-fig = make_subplots(specs=[[{"type": "polar"}]])
-fig.add_trace(
-    go.Scatterpolar(theta=curriculum.values,
-                    r=[0 for v in curriculum.values],
-                    subplot="polar",
-                    fill="toself"), 1, 1)
-fig.update_layout(polar=dict(radialaxis=dict(visible=False)), height=300)
-
-kudos_radar = dcc.Graph(id={
-    "type": "graph",
-    "page": "pastoral",
-    "tab": "kudos",
-},
-                        config={"displayModeBar": False},
-                        figure=fig)
-
-layout = dbc.Row([
-    dbc.Col(width=4, children=[kudos_radar]),
-    dbc.Col(width=8, children=[kudos_table]),
-])
-
+layout = dbc.Container(kudos_table)
 
 @app.callback(
-    [
-        Output({
+    Output(
+        {
             "type": "table",
+            "section": "sixthform",
             "page": "pastoral",
-            "tab": "kudos",
-        }, "data"),
-        Output({
-            "type": "graph",
-            "page": "pastoral",
-            "tab": "kudos",
-        }, "figure")
-    ], [Input("sixthform-pastoral", "data"),
-    ],
-    [State({
-        "type": "graph",
-        "page": "pastoral",
-        "tab": "kudos",
-    }, "figure")])
-def update_pastoral_kudos(store_data, current_figure):
-    enrolment_docs = store_data.get('enrolment_docs')
-    kudos_docs = store_data.get('kudos_docs')
-    if not kudos_docs:
-        current_figure["data"][0]["r"] = [0 for v in curriculum.values]
-        return [], current_figure
-    kudos_df = pd.merge(pd.DataFrame.from_records(enrolment_docs),
-                        pd.DataFrame.from_records(kudos_docs),
-                        how="left",
-                        left_on="_id",
-                        right_on="student_id")
-    kudos_pivot_df = pd.pivot_table(
-        kudos_df,
-        values="points",
-        index=["student_id", "given_name", "family_name"],
-        columns="ada_value",
-        aggfunc=sum,
-        fill_value=0,
-    ).reindex(curriculum.values, axis=1, fill_value=0)
-    kudos_pivot_df["total"] = kudos_pivot_df.sum(axis=1)
-    kudos_pivot_df = kudos_pivot_df.reset_index()
-    r = [kudos_pivot_df[v].sum() for v in curriculum.values]
-    current_figure["data"][0]["r"] = r
-    return kudos_pivot_df.to_dict(orient='records'), current_figure
+            "name": "kudos",
+        }, "data"), [
+            Input("sixthform-pastoral", "data"),
+        ])
+def update_pastoral_kudos(store_data):
+    kudos_pivot_docs = store_data.get('kudos_pivot_docs')
+    if not kudos_pivot_docs:
+        return []
+    return kudos_pivot_docs
