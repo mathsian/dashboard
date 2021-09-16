34ae6beb64bd39ca532a7e554012225e2a12b9e8
Ian Dickerson <ian@ada.ac.uk>
2021-09-16 09:18:29 +0000

    replace dcc dropdowns with dbc dropdowns because their children can be links

--- filters.py
+++ filters.py
@@ -1,5 +1,5 @@
-from urllib.parse import parse_qs, urlparse
-import dash.callback_context as ctx
+from urllib.parse import parse_qs, urlparse, urlencode
+import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
@@ -8,118 +8,8 @@ from dash.dependencies import Input, Output, State, ALL
 from app import app
 import data
 
-cohort = dcc.Dropdown(
-    id={
-        "type": "filter-dropdown",
-        "filter": "cohort"
-    },
-    placeholder="Select a cohort",
-    options=[{
-        "label": s,
-        "value": s
-    } for s in ["1921", "2022", "2123"]],
-    value="2123",
-    clearable=False,
-    persistence=True,
-    persistence_type="local",
-    searchable=False,
-)
-team = dcc.Dropdown(
-    id={
-        "type": "filter-dropdown",
-        "filter": "team"
-    },
-    placeholder="All",
-    persistence=True,
-    persistence_type="local",
-    searchable=False,
-)
-
-subject = dcc.Dropdown(
-    id={
-        "type": "filter-dropdown",
-        "filter": "subject"
-    },
-    placeholder="Select a subject",
-    persistence=True,
-    persistence_type="local",
-    searchable=False,
-)
-
-assessment = dcc.Dropdown(id={
-    "type": "dropdown",
-    "page": "academic",
-    "name": "assessment"
-},
-                                 persistence=True,
-                                 persistence_type='local')
 
 
-@app.callback(
-    Output({
-        "type": "filter-dropdown",
-        "filter": "cohort"
-    }, "value"),
-    [
-        Input({"location": "search"})
-    ],
-    [
-        State({
-        "type": "filter-dropdown",
-        "filter": "cohort"
-        }, "value")
-    ]
-    )
-def update_cohort_value(search, cohort_value):
-    filter_dict = parse_qs(search.removeprefix('?'))
-    cohort_value = filter_dict.get("cohort", cohort_value)
-    return cohort_value
-
-
-@app.callback(
-    [
-        Output({
-            "type": "filter-dropdown",
-            "filter": "team"
-        }, "options"),
-        Output({
-            "type": "filter-dropdown",
-            "filter": "team"
-        }, "value"),
-    ],
-    [
-        Input({"location": "search"}),
-        Input({
-        "type": "filter-dropdown",
-        "filter": "cohort"
-    }, "value")],
-    [
-        State({"history": "data"}),
-        State({"location": "pathname"}),
-        State({
-            "type": "filter-dropdown",
-            "filter": "team"
-        }, "options"),
-        State({
-            "type": "filter-dropdown",
-            "filter": "team"
-        }, "value"),
-
-    ]
-)
-def update_team_filter(search, cohort_value, history, pathname, team_options, team_value):
-    teams = data.get_teams(cohort_value)
-    input_id = ctx.triggered[0]["prop_id"].split(".")[0]
-    if "search" in input_id:
-        search_dict = parse_qs(search.removeprefix('?'))
-        if team := search_dict.get("team", False) and team in teams:
-            history.append(pathname+'?'+search)
-            team_value = team
-        elif team_value not in teams:
-            team_value = teams[0]
-    else:
-
-    return ([{"label": t, "value": t} for t in teams], team_value)
 
 #
 #@app.callback(
