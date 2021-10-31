ae0460a2c37f8b1b2aaa011971e0c8cf4e2efae0
Ian Dickerson <ian@ada.ac.uk>
2021-10-31 19:33:25 +0000

    turn off diagnostics

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ app = dash.Dash(__name__, server=server,
                 suppress_callback_exceptions=True)
 app.title = "data@ada"
 
-app.enable_dev_tools(debug=True, dev_tools_ui=True, dev_tools_props_check=True, dev_tools_serve_dev_bundles=True, dev_tools_hot_reload=True,)
+#app.enable_dev_tools(debug=True, dev_tools_ui=True, dev_tools_props_check=True, dev_tools_serve_dev_bundles=True, dev_tools_hot_reload=True,)
 
 # wrap in google oauth
 auth = GoogleOAuth(app=app,
