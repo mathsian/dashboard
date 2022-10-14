54ed82c6ef15b5b80f8f668a38093c7026624686
Ian Dickerson <ian@ada.ac.uk>
2022-10-14 16:31:31 +0000

    track setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name='dashboard',
+      version='0.7.1',
+      url='https://github.com/mathsian/dashboard.git',
+      author='Ian Dickerson',
+      author_email='ian@ada.ac.uk',
+      description='Dashboard',
+      packages=find_packages())
