c854a6fa3ce9dce150fc733ea1cedbb2a2d6f922
Ian Dickerson <ian@ada.ac.uk>
2023-11-09 15:37:20 +0000

    further date filtering option

--- pages/sixthform_attendance.py
+++ pages/sixthform_attendance.py
@@ -1,12 +1,10 @@
-import pyodbc
-from os.path import abspath
-import jinja2
-from configparser import ConfigParser
 from dash import dcc
 import dash_bootstrap_components as dbc
 import dash_daq as daq
 from dash.dependencies import Input, Output, State, ALL
 import pandas as pd
+
+import data
 from app import app
 from dash_extensions.javascript import Namespace
 
@@ -99,21 +97,7 @@ layout = [
         }, 'n_clicks')
 ])
 def update_attendance_data(n_clicks):
-    config_object = ConfigParser()
-    config_object.read("config.ini")
-    rems_settings = config_object["REMS"]
-    rems_server = rems_settings["ip"]
-    rems_uid = rems_settings["uid"]
-    rems_pwd = rems_settings["pwd"]
-    conn = pyodbc.connect(
-        f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={rems_server};DATABASE=Reports;UID={rems_uid};PWD={rems_pwd}'
-    )
-    sql_jinja_env = jinja2.Environment(
-        loader=jinja2.FileSystemLoader(abspath('.')))
-    sql_template = sql_jinja_env.get_template(
-        'sql/cumulative attendance monthly.sql')
-    sql = sql_template.render()
-    rems_df = pd.read_sql(sql, conn)
+    rems_df = pd.DataFrame.from_records(data.execute_sql_rems('sql/cumulative attendance monthly.sql'))
 
     cumulative_value = rems_df.query(
         'student_id == "All" & date == "Year"')["attendance"].iat[0]
