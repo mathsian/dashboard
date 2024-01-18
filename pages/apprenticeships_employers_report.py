485106e9a720ca1bb77b1d34db108a60688c5140
Ian Dickerson <ian@ada.ac.uk>
2024-01-18 16:14:25 +0000

    use dash mantine components for more control of accordion headings

--- pages/apprenticeships_employers_report.py
+++ pages/apprenticeships_employers_report.py
@@ -14,6 +14,7 @@ from urllib.parse import parse_qs, urlencode
 from app import app
 import app_data
 import dash_bootstrap_components as dbc
+import dash_mantine_components as dmc
 
 layout = dbc.Row(
     dbc.Col(
@@ -25,7 +26,7 @@ layout = dbc.Row(
                 "tab": "report",
                 "name": "reports"
             }),
-         dbc.Accordion(
+         dmc.Accordion(
   id={
                 "section": "apprenticeships",
                 "type": "accordion",
@@ -72,64 +73,67 @@ def update_student_report(store_data):
 
         results, credits, average_result = app_data.get_results_report_for_student(student_id, student.get('top_up', False))
 
-        accordion_item = dbc.AccordionItem(
-            id={
+        accordion_item = dmc.AccordionItem(children=[
+            dmc.AccordionControl(f'{student.get("given_name")} {student.get("family_name")}'),
+            dmc.AccordionPanel(id={
                 "type": "accordion_item",
                 "section": "apprenticeships",
                 "page": "cohorts",
                 "tab": "report",
                 "name": f'{student_id}'
             },
-            children=[
-                dbc.Row([
-                    dbc.Col([
-                        dbc.Table([
-                            html.Tr([
-                                html.Td("Student ID"),
-                                html.Td(student_id)
-                            ]),
-                            html.Tr([
-                                html.Td("Cohort"),
-                                html.Td(student.get("cohort_name"))
-                            ]),
-                            html.Tr([
-                                html.Td("Employer"),
-                                html.Td(student.get("employer"))
-                            ]),
-                            html.Tr([
-                                html.Td("Status"),
-                                html.Td(student.get("status"))
-                            ]),
-                            html.Tr([html.Td("Credits"),
-                                     html.Td(credits)
-                            ]),
-                            html.Tr([html.Td("Average"),
-                                     html.Td(average_result)])
-                        ],
-                                  borderless=True,
-                                  hover=True,
-                                  responsive=True,
-                                  striped=True),
-                    ]),
-                    dbc.Col([
-                        dbc.Table([
-                            html.Tr([
-                                html.Td(r.get("level", "-")),
-                                html.Td(r.get("name", "-")),
-                                html.Td(
-                                    html.A(r.get("code"), href=f'/apprenticeships/academic/edit?module={r.get("short")}&instance={r.get("code")}')
-                                ),
-                                html.Td(r.get("total", "-"))
-                            ]) for r in results
-                        ],
-                                  bordered=True,
-                                  hover=True,
-                                  responsive=True,
-                                  striped=True)
+                children=[
+                    dbc.Row([
+                        dbc.Col([
+                            dbc.Table([
+                                html.Tr([
+                                    html.Td("Student ID"),
+                                    html.Td(student_id)
+                                ]),
+                                html.Tr([
+                                    html.Td("Cohort"),
+                                    html.Td(student.get("cohort_name"))
+                                ]),
+                                html.Tr([
+                                    html.Td("Employer"),
+                                    html.Td(student.get("employer"))
+                                ]),
+                                html.Tr([
+                                    html.Td("Status"),
+                                    html.Td(student.get("status"))
+                                ]),
+                                html.Tr([html.Td("Credits"),
+                                         html.Td(credits)
+                                         ]),
+                                html.Tr([html.Td("Average"),
+                                         html.Td(average_result)])
+                            ],
+                                borderless=True,
+                                hover=True,
+                                responsive=True,
+                                striped=True),
+                        ]),
+                        dbc.Col([
+                            dbc.Table([
+                                html.Tr([
+                                    html.Td(r.get("level", "-")),
+                                    html.Td(r.get("name", "-")),
+                                    html.Td(
+                                        html.A(r.get("code"),
+                                               href=f'/apprenticeships/academic/edit?module={r.get("short")}&instance={r.get("code")}')
+                                    ),
+                                    html.Td(r.get("total", "-"))
+                                ]) for r in results
+                            ],
+                                bordered=True,
+                                hover=True,
+                                responsive=True,
+                                striped=True)
+                        ])
                     ])
                 ])
-            ],
-            title=f'{student.get("given_name")} {student.get("family_name")}',
+        ],
+            value=f'{student.get("given_name")} {student.get("family_name")}'
         )
         accordion_items.append(accordion_item)
 
